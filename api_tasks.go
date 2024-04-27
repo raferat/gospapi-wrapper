b989b5ffd20b177f5f5c6d5e0696eeeb86d966d1
Raferat <mailnaneduveryhodnestranky@seznam.cz>
2024-04-27 19:07:19 +0200

    Partial cleanup api_tasks.go - TasksListGet

--- api_tasks.go
+++ api_tasks.go
@@ -149,71 +149,51 @@ type TasksApiTasksListGetOpts struct {
 	Set optional.String
 }
 
-func (a *TasksApiService) TasksListGet(ctx context.Context, localVarOptionals *TasksApiTasksListGetOpts) ([]string, *http.Response, error) {
+func (serv *TasksApiService) TasksListGet(ctx context.Context, requestOptions *TasksApiTasksListGetOpts) ([]string, *http.Response, error) {
 	var (
-		localVarHttpMethod  = strings.ToUpper("Get")
-		localVarPostBody    interface{}
-		localVarFileName    string
-		localVarFileBytes   []byte
-		localVarReturnValue []string
+		requestBody    interface{}
+		parsedResponse []string
 	)
 
 	// create path and map variables
-	localVarPath := a.client.cfg.BasePath + "/tasks/list"
+	requestPath := serv.client.cfg.BasePath + "/tasks/list"
 
-	localVarHeaderParams := make(map[string]string)
-	localVarQueryParams := url.Values{}
-	localVarFormParams := url.Values{}
+	requestHeaders := make(map[string]string)
+	requestQueryParams := url.Values{}
 
-	if localVarOptionals != nil && localVarOptionals.Set.IsSet() {
-		localVarQueryParams.Add("set", parameterToString(localVarOptionals.Set.Value(), ""))
+	if requestOptions != nil && requestOptions.Set.IsSet() {
+		requestQueryParams.Add("set", parameterToString(requestOptions.Set.Value(), ""))
 	}
 
-	localVarHeaderParams["Accept"] = "application/json"
+	requestHeaders["Accept"] = "application/json"
 
-	r, err := a.client.prepareRequest(ctx, localVarPath, localVarHttpMethod, localVarPostBody, localVarHeaderParams, localVarQueryParams, localVarFormParams, localVarFileName, localVarFileBytes)
+	r, err := serv.client.prepareRequest(ctx, requestPath, "GET", requestBody, requestHeaders, requestQueryParams, url.Values{}, "", []byte{})
 	if err != nil {
-		return localVarReturnValue, nil, err
+		return parsedResponse, nil, err
 	}
 
-	localVarHttpResponse, err := a.client.callAPI(r)
-	if err != nil || localVarHttpResponse == nil {
-		return localVarReturnValue, localVarHttpResponse, err
+	rawResponse, err := serv.client.callAPI(r)
+	if err != nil || rawResponse == nil {
+		return parsedResponse, rawResponse, err
 	}
 
-	localVarBody, err := io.ReadAll(localVarHttpResponse.Body)
-	localVarHttpResponse.Body.Close()
+	responseBody, err := io.ReadAll(rawResponse.Body)
+	rawResponse.Body.Close()
 	if err != nil {
-		return localVarReturnValue, localVarHttpResponse, err
+		return parsedResponse, rawResponse, err
 	}
 
-	if localVarHttpResponse.StatusCode < 300 {
+	if rawResponse.StatusCode < 300 {
 		// If we succeed, return the data, otherwise pass on to decode error.
-		err = a.client.decode(&localVarReturnValue, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-		if err == nil {
-			return localVarReturnValue, localVarHttpResponse, err
-		}
+		err = serv.client.decode(&parsedResponse, responseBody, rawResponse.Header.Get("Content-Type"))
+		return parsedResponse, rawResponse, err
 	}
 
-	if localVarHttpResponse.StatusCode >= 300 {
-		newErr := GenericSwaggerError{
-			body:  localVarBody,
-			error: localVarHttpResponse.Status,
-		}
-		if localVarHttpResponse.StatusCode == 200 {
-			var v []string
-			err = a.client.decode(&v, localVarBody, localVarHttpResponse.Header.Get("Content-Type"))
-			if err != nil {
-				newErr.error = err.Error()
-				return localVarReturnValue, localVarHttpResponse, newErr
-			}
-			newErr.model = v
-			return localVarReturnValue, localVarHttpResponse, newErr
-		}
-		return localVarReturnValue, localVarHttpResponse, newErr
+	newErr := GenericSwaggerError{
+		body:  responseBody,
+		error: rawResponse.Status,
 	}
-
-	return localVarReturnValue, localVarHttpResponse, nil
+	return parsedResponse, rawResponse, newErr
 }
 
 /*
